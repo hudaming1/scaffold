b38570ca24c46289098e80b8f15939a9f52c942b
huming <512657752@qq.com>
2019-10-31 09:12:35 +0800

    testcase

---
+++ src/test/java/org/hum/scaffold/ZkClientTest.java
@@ -0,0 +1,22 @@
+package org.hum.scaffold;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
+
+public class ZkClientTest {
+
+	public static void main(String[] args) {
+		ZkClient zkClient = new ZkClient("10.2.3.83:2181");
+		List<String> children = zkClient.getChildren("/testshardingesjob/smartTestJob1/sharding/0");
+		children.forEach(path -> {
+			System.out.println(path);
+		});
+		
+		Entry<List<ACL>, Stat> acl = zkClient.getAcl("/testshardingesjob/smartTestJob1/sharding/0");
+		System.out.println(acl);
+	}
+}
